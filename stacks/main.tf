91c8236d23cdb6b63f913554e075d6315dabe14b
Matthew <mwimpelberg11@proton.me>
2023-03-22 05:20:23 -0400

    adding constraint for version

--- stacks/main.tf
+++ stacks/main.tf
@@ -7,6 +7,6 @@ resource "spacelift_stack" "k8s-cluster" {
   name              = "Kubernetes Cluster"
   project_root      = "ec2_instance/"
   repository        = "spacelift-poc"
-  terraform_version = "1.4.2"
+  terraform_version = "1.4.0"
   space_id = "dev-01GW05MHFJSEA7ZCA7TKKSAGKD"
 }
